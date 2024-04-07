730f3b64790a0515378ea7133c01afa3edc8739d
Jovan Shelomo <jovanshelomo@gmail.com>
2024-04-07 16:58:31 +0700

    update

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
 import { calculate } from './src/calculate.js';
-const stdinIterator = process.stdin.iterator()
 
-try {
-    process.stdout.write("Masukkan angka pertama: ");
-    const input1 = ((await stdinIterator.next()).value).toString().trim()
-    process.stdout.write("Masukkan angka kedua: ");
-    const input2 = ((await stdinIterator.next()).value).toString().trim()
-    process.stdout.write("Masukkan operator (+, -, *, /): ");
-    const operator = ((await stdinIterator.next()).value).toString().trim()
-    const a = parseInt(input1);
-    const b = parseInt(input2);
+export async function main() {
+    const stdinIterator = process.stdin.iterator()
 
-    console.log("hasil: " + calculate(a, b, operator))
-} catch (error) {
-    console.error(error.message)
+    try {
+        process.stdout.write("Masukkan angka pertama: ");
+        const input1 = ((await stdinIterator.next()).value).toString().trim()
+        process.stdout.write("Masukkan angka kedua: ");
+        const input2 = ((await stdinIterator.next()).value).toString().trim()
+        process.stdout.write("Masukkan operator (+, -, *, /): ");
+        const operator = ((await stdinIterator.next()).value).toString().trim()
+
+        console.log("hasil: " + calculate(input1, input2, operator))
+    } catch (error) {
+        console.log(error.message)
+    }
 }
+main();
