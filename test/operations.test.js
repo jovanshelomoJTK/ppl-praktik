f8e38985a76b669e2f8dfd991a01a6599bffb998
erhaemael <rahma.alia.tif421@polban.ac.id>
2024-04-21 16:26:30 +0700

    update test case

--- test/operations.test.js
+++ test/operations.test.js
@@ -1,17 +1,9 @@
 import { add, divide, multiply, subtract } from '../src/operations.js'
 import { describe, expect, it } from 'vitest';
 
-describe('Test add function', () => {
+describe('Operasi penambahan 2 angka', () => {
     it('adds 1 + 2 to equal 3', () => {
         expect(add(1, 2)).toBe(3);
-    });
-
-    it('adds 123 + 456 to equal 579', () => {
-        expect(add(123, 456)).toBe(579);
-    });
-
-    it('adds 0 + 0 to equal 0', () => {
-        expect(add(0, 0)).toBe(0);
     });
 });
 
@@ -43,18 +35,14 @@ describe('Test multiply function', () => {
     });
 })
 
-describe('Test divide function', () => {
+describe('Operasi pembagian 2 angka', () => {
     it('divide 6 / 2 to equal 3', () => {
         expect(divide(6, 2)).toBe(3);
     });
 
-    it('divide 0 / 123 to equal 0', () => {
-        expect(divide(0, 123)).toBe(0);
-    });
-
-    it('divide 123 / 0 to throw an error', () => {
+    it('Operasi pembagian dengan pembilang nol', () => {
         expect(() => {
-            divide(123, 0)
+            divide(6, 0)
         }).toThrowError('Tidak bisa membagi dengan 0!');
     });
 });
